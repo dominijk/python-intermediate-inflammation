b4cb83671ff97ed4930ef3fbc359575c4ce0be84
dominijk <dominijk@gmail.com>
2021-07-01 13:28:24 +0100

    Fixes #1

--- tests/test_models.py
+++ tests/test_models.py
@@ -53,26 +53,33 @@ def test_daily_min_string():
         (
             [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
             [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            None,
         ),
         (
             [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
             [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None,
         ),
         (
             [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
             [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+            None,
         ),
         (
             [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
             [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+            None,
+
         ),
         (
             [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
             [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
         ),
         (
             [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
             [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
         ),
         (
             [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
@@ -85,7 +92,7 @@ def test_daily_min_string():
             None,
         ),
     ])
-def test_patient_normalise(test, expected):
+def test_patient_normalise(test, expected,raises):
     """Test normalisation works for arrays of one and positive integers."""
     from inflammation.models import patient_normalise
     if raises:
@@ -94,3 +101,4 @@ def test_patient_normalise(test, expected):
     else:
         npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
 # TODO(lesson-robust) Implement tests for the other statistical functions
+
